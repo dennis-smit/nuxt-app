1d118e7d62b0ccaebb83f8ee3d181a2c42649777
Dennis Smit <frontenddeveloper@outlook.com>
2019-08-01 15:36:53 +0200

    Axios API Call

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,39 @@
 <template>
-  <div><h1>Events</h1></div>
+  <div>
+    <h1>Events</h1>
+    <EventCard
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    />
+  </div>
 </template>
-
 <script>
+import EventCard from '@/components/EventCard.vue'
 export default {
   head() {
     return {
       title: 'Event Listing'
     }
+  },
+  components: {
+    EventCard
+  },
+  asyncData({ $axios, error }) {
+    return $axios
+      .get('http://localhost:3000/events')
+      .then((response) => {
+        return {
+          events: response.data
+        }
+      })
+      .catch((e) => {
+        error({
+          statusCode: 503,
+          message: 'Unable to fetch events at this time, please try again'
+        })
+      })
   }
 }
 </script>
-
-<style lang="scss" scoped></style>
