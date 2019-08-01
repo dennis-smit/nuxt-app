061093ef3b59441099e25a0d44b264dba004d8f8
Dennis Smit <frontenddeveloper@outlook.com>
2019-08-01 14:15:28 +0200

    SEO

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div><h1>Events</h1></div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Event Listing'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
